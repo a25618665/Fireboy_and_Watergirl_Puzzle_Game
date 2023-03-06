d1093fa7d943414e78d6e24f280ab5086207e883
yente  liu <t109820024@ntut.org.tw>
2023-03-06 10:04:01 +0800

    deleting my unnecessary  directory

--- main_game/Source/Game/mygame_initialize.cpp
+++ main_game/Source/Game/mygame_initialize.cpp
@@ -45,7 +45,7 @@ void CGameStateInit::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
 
 void CGameStateInit::OnLButtonDown(UINT nFlags, CPoint point)
 {
-	GotoGameState(GAME_STATE_RUN);		// 切換至GAME_STATE_RUN
+	//GotoGameState(GAME_STATE_RUN);		// 切換至GAME_STATE_RUN
 }
 
 void CGameStateInit::OnShow()
