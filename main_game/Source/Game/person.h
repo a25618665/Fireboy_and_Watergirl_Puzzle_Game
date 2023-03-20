343f63415138f194a6551e0e3ecf6ba9bac6120b
fl <109820020@ntut.org.tw>
2023-03-20 18:44:41 +0800

    complete jump

--- main_game/Source/Game/person.h
+++ main_game/Source/Game/person.h
@@ -11,11 +11,16 @@ namespace game_framework {
 		void setMovingRight(bool flag);
 		int getX();
 		int getY();
+		void jump();
 		void OnMove();
 		void OnShow();
 
 		bool is_boy;   //True: boy, False: girl
 		int x, y;
+		int jump_iterator;
+		const int jump_velocity[33] = { 8, 8, 7, 7, 6, 6, 5, 5, 4, 4, 3, 3, 2, 2, 1, 1, 0, 
+			                           -1, -1, -2, -2, -3, -3, -4, -4, -5, -5, -6, -6, -7, -7, -8, -8};
+		bool is_jumping;
 		bool is_moving_left;
 		bool is_moving_right;
 		CMovingBitmap img_stop;
