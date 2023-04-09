a1c2ee12a03232e5b22114b37926d3337ce0e16f
yente  liu <t109820024@ntut.org.tw>
2023-04-09 22:46:22 +0800

    purple platform and bytton is    ready

--- main_game/Source/Game/GameObject/platform.h
+++ main_game/Source/Game/GameObject/platform.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "switch.h"
+#include "button.h"
 namespace game_framework {
 	class PlatForm {
 	public:
 		PlatForm();
 		void OnShow();
 		void OnMove(const  Switch  &switch_add);
+		void OnMove(const Button &button );
 		void init(int x, int y ,int Y_END , int  color );//1:purple2:yellow3:green
 		int GetX();
 		int GetY();
