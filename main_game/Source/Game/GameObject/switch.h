10a9f1fbfe3902ab84a0ef009cfa7593a89d616a
fl <109820020@ntut.org.tw>
2023-04-14 09:56:00 +0800

    before change map

--- main_game/Source/Game/GameObject/switch.h
+++ main_game/Source/Game/GameObject/switch.h
@@ -1,38 +1,21 @@
 #pragma once
+
 namespace game_framework {
-	
 	class Switch {
 	public:
 		Switch();
+		void Init(int x, int y, int color);
 		void OnShow();
 		void OnMove(const CRect & person_body);
-		void init(int x, int y , int color);
+		
 		int GetX();
 		int GetY();
+	private:
 		bool is_right;
 		int x, y ,color;/// color 0: yellow 
 		int moving_status; //1:left 0:right 
 		CMovingBitmap right;
 		CMovingBitmap left;
 		CRect body;
-	
 	};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
