daf2a43cd6e03ce0a6d894e84d0a2a0c666dd8b9
yente  liu <t109820024@ntut.org.tw>
2023-05-11 20:38:26 +0800

    purple platform and bytton is    ready

--- main_game/Source/Game/GameObject/timer.h
+++ main_game/Source/Game/GameObject/timer.h
@@ -8,12 +8,6 @@ namespace game_framework {
 		static void show_in_the_game(int time_counter);
 		static void show_as_text(int  text, int x, int y); //x,y分別為要擺放的位置text為內容
 		static void show_as_text(string text, int x, int y); //x,y分別為要擺放的位置text為內容
-	private:
-		//static int minute;
-		//static int second; 
-		//static int blue_diamond;
-		//static int red_diamond; //用來決定顯示寶石數目及時間換算變數
-		
 								
 	};
 }
