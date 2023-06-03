43c57fcfe9c0e1e7cd48cb4ef1747e04b6753a60
fl <109820020@ntut.org.tw>
2023-06-03 20:50:44 +0800

    0603

--- main_game/Source/Game/GameObject/platform.h
+++ main_game/Source/Game/GameObject/platform.h
@@ -9,7 +9,8 @@ namespace game_framework {
 	public:
 		Platform();			
 		// Init: end: 移動完的座標(x或y), direction: 'U', 'D', 'L', 'R', color: 'Y', 'P' ,'B', plat_dir: 'H', 'V'
-		void Init(int x, int y, int end, char defult_direction, char color, array<array<int, 480>, 640> *map, char plat_dir = 'H');
+		void Init(int x, int y, int end, char defult_direction, char color, array<array<int, 480>, 640> *map, 
+				  char plat_dir = 'H', char len = 'S');
 		void Bind(Switch *ptr_switch);
 		void Bind(Button *ptr_button);							// 只有被一個buttton控制
 		void Bind(const vector<Button *> & button_ptr_vector);	// 被兩個以上buttton控制
@@ -23,6 +24,7 @@ namespace game_framework {
 		int original_x, original_y, x, y, end;
 		char defult_direction;
 		char plat_dir;
+		char len;
 		CMovingBitmap img;
 		array<array<int, 480>, 640> *ptr_map;
 		Switch *ptr_switch;
