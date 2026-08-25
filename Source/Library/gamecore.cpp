0e14a20210beefcf5c21f9714a64ed3d456f45ed
yente liu <a25618665@gmail.com>
2026-08-25 16:24:53 -0700

    Cleanup/source metadata (#2)
    * docs: correct project metadata and remove obsolete includes
    
    * style: remove obsolete includes and normalize headers
    
    * style: remove obsolete filesystem namespace

--- Source/Library/gamecore.cpp
+++ Source/Library/gamecore.cpp
@@ -13,10 +13,7 @@
 #include "Shlwapi.h"
 #include "../Game/config.h"
 #include "../Game/mygame.h"
-//#include <filesystem>
-//#include <experimental/filesystem> // Header file for pre-standard implementation
 #include <comdef.h>
-//using namespace std::experimental::filesystem::v1;
 
 
 namespace game_framework {
